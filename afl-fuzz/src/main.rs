57201d0e3a5df3277eb87c8fda539c055dffdef4
Min Deng <minotes.net@gmail.com>
2024-09-18 08:35:54 +0800

    fuzz tests updated

--- afl-fuzz/src/main.rs
+++ afl-fuzz/src/main.rs
@@ -1,22 +1,52 @@
 use std::io::Cursor;
 
-use nom_exif::{
-    parse_exif, parse_heif_exif, parse_jpeg_exif, parse_metadata, parse_mov_metadata, Exif,
-};
+use nom_exif::{Exif, ExifIter, MediaParser, MediaSource, TrackInfo};
 
 fn main() {
     afl::fuzz!(|data: &[u8]| {
+        let mut parser = MediaParser::new();
+
+        // MediaSource
+
         let reader = Cursor::new(data);
-        let iter = parse_exif(reader.clone(), None);
+        let _ = MediaSource::seekable(reader);
+        let reader = Cursor::new(data);
+        let _ = MediaSource::unseekable(reader);
+
+        // Parse seekable
+
+        let reader = Cursor::new(data);
+        let Ok(ms) = MediaSource::seekable(reader) else {
+            return;
+        };
+        let iter: Result<ExifIter, _> = parser.parse(ms);
         if let Ok(iter) = iter {
-            if let Some(iter) = iter {
-                let _ = iter.parse_gps_info();
-                let _: Exif = iter.into();
-            }
+            let _ = iter.parse_gps_info();
+            let _: Exif = iter.into();
         }
-        let _ = parse_metadata(reader.clone());
-        let _ = parse_heif_exif(reader.clone());
-        let _ = parse_jpeg_exif(reader.clone());
-        let _ = parse_mov_metadata(reader.clone());
+
+        let reader = Cursor::new(data);
+        let Ok(ms) = MediaSource::seekable(reader) else {
+            return;
+        };
+        let _: Result<TrackInfo, _> = parser.parse(ms);
+
+        // Parse unseekable
+
+        let reader = Cursor::new(data);
+        let Ok(ms) = MediaSource::unseekable(reader) else {
+            return;
+        };
+        let iter: Result<ExifIter, _> = parser.parse(ms);
+        if let Ok(iter) = iter {
+            let _ = iter.parse_gps_info();
+            let _: Exif = iter.into();
+        }
+
+        let reader = Cursor::new(data);
+        let Ok(ms) = MediaSource::unseekable(reader) else {
+            return;
+        };
+        let _: Result<TrackInfo, _> = parser.parse(ms);
     });
 }
